64ad97fa02e5f1f01067b102b69efe4e9ade07d6
Salah0123 <hossameldinsalah67@gmail.com>
2023-06-07 16:55:35 +0200

    hossam v1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CartShoppingComponent } from './components/cart-shopping/cart-shopping.component';
+import { AllProductsComponent } from './all-products/all-products.component';
 
 const routes: Routes = [
-  {path:'shoppingCart', component:CartShoppingComponent}
+  {path:'shoppingCart', component:CartShoppingComponent},
+  {path:"products", component:AllProductsComponent}
 ];
 
 @NgModule({
