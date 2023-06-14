e8df606868ccc4cf4f9b82ea0fc97fb674bb6bcd
mostafaXsameer99 <mostafa19w99sameer@gmail.com>
2023-06-14 20:40:56 +0300

    انا الفاااااجر

--- src/app/components/cart-shopping/cart-shopping.component.ts
+++ src/app/components/cart-shopping/cart-shopping.component.ts
@@ -6,5 +6,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./cart-shopping.component.scss']
 })
 export class CartShoppingComponent {
+  selectedSize: string = 'M';
+  Quantity: number = 1;
+  count: number = 1
+  cart: any[] = [1,2];
 
+  get cartSize(): number {
+    console.log(this.cart.length);
+
+    return this.cart.length;
+  }
+
+  selectSize(size: string) {
+    this.selectedSize = size;
+    console.log('Selected size:', this.selectedSize);
+  }
+  removeItem(id: any) {
+    console.log('Remove item', event);
+  }
+  plusOne() {
+    this.Quantity++;
+  }
+  minusOne() {
+    if (this.Quantity > 1) { this.Quantity--; }
+  }
+  completeOrder() {
+    console.log("send data to database");
+
+  }
 }
